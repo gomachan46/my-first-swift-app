289de15d317ce8cb04510407eae49f9b68ca88d8
gomachan46 <shiro.gomachan46@gmail.com>
2017-03-11 23:14:26 +0900

    電卓で一通り計算できるようになった

--- calc/calc/ViewController.swift
+++ calc/calc/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  @IBOutlet weak var priceField: UITextField!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +21,64 @@ class ViewController: UIViewController {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
-
-
+  
+  @IBAction func tap1Button(_ sender: Any) {
+    updatePrice(price: currentPrice() + 1)
+  }
+  
+  @IBAction func tap2Button(_ sender: Any) {
+    updatePrice(price: currentPrice() + 2)
+  }
+  
+  @IBAction func tap3Button(_ sender: Any) {
+    updatePrice(price: currentPrice() + 3)
+  }
+  
+  @IBAction func tap4Button(_ sender: Any) {
+    updatePrice(price: currentPrice() + 4)
+  }
+  
+  @IBAction func tap5Button(_ sender: Any) {
+    updatePrice(price: currentPrice() + 5)
+  }
+  
+  @IBAction func tap6Button(_ sender: Any) {
+    updatePrice(price: currentPrice() + 6)
+  }
+  
+  @IBAction func tap7Button(_ sender: Any) {
+    updatePrice(price: currentPrice() + 7)
+  }
+  
+  @IBAction func tap8Button(_ sender: Any) {
+    updatePrice(price: currentPrice() + 8)
+  }
+  
+  @IBAction func tap9Button(_ sender: Any) {
+    updatePrice(price: currentPrice() + 9)
+  }
+  
+  @IBAction func tap0Button(_ sender: Any) {
+    updatePrice(price: currentPrice() * 10)
+  }
+  
+  @IBAction func tap00Button(_ sender: Any) {
+    updatePrice(price: currentPrice() * 100)
+  }
+  
+  @IBAction func tapCButton(_ sender: Any) {
+    updatePrice(price: 0)
+  }
+  
+  private func updatePrice(price: Int) {
+    priceField.text = "\(price)"
+  }
+  
+  private func currentPrice() -> Int {
+    guard let currentPrice = Int(priceField.text!) else {
+      return 0
+    }
+    
+    return currentPrice
+  }
 }
-
